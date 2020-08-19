d83ed286ec4856f2afa98b39504b0f50d5422885
blueming29 <blueming29@naver.com>
2020-08-19 17:25:05 +0900

    score_step2

--- src/com/tech1/score/ScoreMgr.java
+++ src/com/tech1/score/ScoreMgr.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class ScoreMgr {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int sum = 0;
-		float avg = 0.0f;
 		int[] ScoreArr = new int[5];
-		System.out.println("5명의 점수를 입력하세요");
 
-		for (int i = 0; i < ScoreArr.length; i++) {
-			ScoreArr[i] = sc.nextInt();
+		while (true) {
+			int sum = 0;
+			float avg = 0.0f;
+			System.out.println("5명의 점수를 입력하세요(종료 : -1)");
+			for (int i = 0; i < ScoreArr.length; i++) {
+				ScoreArr[i] = sc.nextInt();
+				if (ScoreArr[i] == -1) {
+					System.out.println("종료합니다");
+					return;
+				} else if((ScoreArr[i]<50) || (ScoreArr[i]>100)) {
+					System.out.println("다시 입력하기\n");
+					break;
+				} else {
+					sum += ScoreArr[i];					
+				}
+			}
+			
+			avg = (float) sum / ScoreArr.length;
+
+			System.out.println("합계 : " + sum + "\n평균 " + avg);
+			System.out.println();
 		}
-
-		for (int i = 0; i < ScoreArr.length; i++) {
-			sum += ScoreArr[i];
-		}
-		avg = (float) sum / ScoreArr.length;
-
-		System.out.println("합계 : " + sum + "\n평균 " + avg);
-		sc.close();
 	}
 }
